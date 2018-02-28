840cef44594954dda1357d405d5a974b5bd6ef95
Min Bao <min_bao@symeowin.local>
2018-02-28 16:19:05 +0100

    bgt24 Menu was removed

--- stm32f4xx_spi_uart/Src/rs232_menu.c
+++ stm32f4xx_spi_uart/Src/rs232_menu.c
@@ -30,10 +30,9 @@ struct rs232_menu{
 };
 
 static struct rs232_menu mainMenu = {"Main Menu",
-	3,
+	2,
 	{"DAC value setting",
-	"ADF4159 configuration",
-	"BGT24MTR11 configuration"}
+	"ADF4159 configuration"}
 };
 
 static struct rs232_menu dacMenu = {"DAC value setting",
@@ -54,12 +53,6 @@ static struct rs232_menu adf4159Menu = {"ADF4159 configuration",
 	}
 };
 
-static struct rs232_menu bgt24Menu = {"BGT24MTR11 configuration",
-	2,
-	{"BGT24 SPI configuration",
-	 "back"
-	}
-};
 
 
 //Main menu
@@ -68,7 +61,6 @@ static void mainMenuExcution(const struct rs232_menu *menu);
 /*Submenu*/
 static void dacMenuExcution(const struct rs232_menu *menu);
 static void adf4159MenuExcution(const struct rs232_menu *menu);
-static void bgt24MenuExcution(const struct rs232_menu *menu);
 
 /*the functions of submenu*/
 static void dacAddtion(void);
@@ -79,7 +71,7 @@ static void dacDivision(void);
 static void adf4159Spi1PllConfig(void);
 static void adf4159RampSet(void);
 
-static void bgt24Spi1Config(void);
+static void bgt24Init(void);
 
 /*the tranmission functions*/
 static void Spi3DacTran(void);
@@ -129,12 +121,10 @@ static void mainMenuExcution(const struct rs232_menu *menu)
 			dacMenuExcution(&dacMenu);
 			break;
 		case 2:
+			bgt24Init();
 			adf4159MenuExcution(&adf4159Menu);
-			break;
-		case 3:
-			bgt24MenuExcution(&bgt24Menu);
 		}
-	} while (optionSelect < 1 || optionSelect > 3);
+	} while (optionSelect < 1 || optionSelect > 2);
 }
 
 static void dacMenuExcution(const struct rs232_menu *menu) {
@@ -225,39 +215,6 @@ static void adf4159MenuExcution(const struct rs232_menu *menu) {
 	} while (optionSelect < 1 || optionSelect > 3);
 }
 
-static void bgt24MenuExcution(const struct rs232_menu *menu) {
-	do {
-		//print function through uart1
-		bufferLen = sprintf(uart1BufferTx, "%s\n\r", menu->menuName);
-		HAL_UART_Transmit(&huart1, (uint8_t *) uart1BufferTx, bufferLen, 1000);
-
-		for (int i = 0; i < menu->menuLength; i++) {
-			bufferLen = sprintf(uart1BufferTx, "%d: %s\n\r", i + 1,
-					menu->menuOption[i]);
-			HAL_UART_Transmit(&huart1, (uint8_t *) uart1BufferTx, bufferLen,
-					1000);
-		};
-
-		bufferLen = sprintf(uart1BufferTx, "\n\r");
-		HAL_UART_Transmit(&huart1, (uint8_t *) uart1BufferTx, bufferLen, 1000);
-
-		do {
-			HAL_UART_Receive_IT(&huart1, (uint8_t *) &uart1DataRx, 1); //active the uart1 to accept interrupt
-			while (!uart1TxCplt) {
-			};				//stop the program until the interrupt is accepted
-
-			optionSelect = uart1DataRx - '0';		//convert the char to int
-			uart1TxCplt = 0;
-			switch (optionSelect) {
-			case 1:
-				bgt24Spi1Config();
-				break;
-			case 2:
-				mainMenuExcution(&mainMenu);
-			}
-		} while (optionSelect > 0 && optionSelect <= menu->menuLength);
-	} while (optionSelect < 1 || optionSelect > 2);
-}
 
 
 static void dacAddtion(void){
@@ -305,31 +262,28 @@ static void dacDivision(void){
 
 
 static void adf4159Spi1PllConfig(void){
-//	MX_SPI1_BGT24_Init();
-//		Spi1Bgt24Tran();
-//		bufferLen = sprintf(uart1BufferTx, "BGT24 was configured\n\r");
-//		HAL_UART_Transmit(&huart1, (uint8_t *)uart1BufferTx, bufferLen, 1000);
+
 	MX_SPI1_ADF4159_Init();
-//	adf4159Spi1Tx(5, 3073829280); //only for test
-//	adf4159Spi1Tx(PLL_REG_R7_DELAY, 1L << R7_SHIFT_TX_DATA_TRIGGER); 					//TX_DATA_TRIGGER enabled
-//	adf4159Spi1Tx(PLL_REG_R6_STEP, 0L << R6_SHIFT_STEP_SEL);
-//	adf4159Spi1Tx(PLL_REG_R6_STEP, 1L << R6_SHIFT_STEP_SEL); 							// second step register is not used
-//	adf4159Spi1Tx(PLL_REG_R5_DEVIATION, 0L << R5_SHIFT_DEV_SEL);
-//	adf4159Spi1Tx(PLL_REG_R5_DEVIATION, 1L << R5_SHIFT_DEV_SEL);							// second deviation register is not used
-//	adf4159Spi1Tx(PLL_REG_R4_CLOCK, 0L << R4_SHIFT_CLK_DIV_SEL);
-//	adf4159Spi1Tx(PLL_REG_R4_CLOCK, 1L << R4_SHIFT_CLK_DIV_SEL);
-//	adf4159Spi1Tx(PLL_REG_R3_FUNCTION, (6L << R3_SHIFT_NEG_BLEED_CURRENT)				// negative bleed current 454.7 uA; but not actually enabled
-//									| (1L << R3_SHIFT_RESERVED_1)						// reserved; must be 1
-//									| (1L << R3_SHIFT_LOL)								// disable "loss of lock" feature; not using it makes things supposedly more robust
-//									| (1L << R3_SHIFT_N_SEL)							// 1 = delay N word load by 4 cycles; makes transition between sweeps smoother
-//									| (1L << R3_SHIFT_LDP)								// "lock detect precision"; writing 1makes things supposedly more robust
-//									| (1L << R3_SHIFT_SIGMA_DELTA_RESET)				// 1 = disable sigma-delta reset on every write
-//									| (0L << R3_SHIFT_PD_POL));							// phase detector polarity is negativ
-//	adf4159Spi1Tx(PLL_REG_R2_R_DIVIDER, (7L << R2_SHIFT_CP_CURRENT_SETTING)				// CP current 2.5 mA
-//									| (R2_REFERENCE_DIV2_VALUE << R2_SHIFT_RDIV2)
-//									| (R2_REF_IN_DOUBLER_VALUE << R2_SHIFT_REF_DOUBLER)
-//									| (R2_R_COUNTER_VALUE << R2_SHIFT_R_COUNTER)
-//									| (0L << R2_SHIFT_CLK1_DIV_VALUE));
+	adf4159Spi1Tx(5, 3073829280); //only for test
+	adf4159Spi1Tx(PLL_REG_R7_DELAY, 1L << R7_SHIFT_TX_DATA_TRIGGER); 					//TX_DATA_TRIGGER enabled
+	adf4159Spi1Tx(PLL_REG_R6_STEP, 0L << R6_SHIFT_STEP_SEL);
+	adf4159Spi1Tx(PLL_REG_R6_STEP, 1L << R6_SHIFT_STEP_SEL); 							// second step register is not used
+	adf4159Spi1Tx(PLL_REG_R5_DEVIATION, 0L << R5_SHIFT_DEV_SEL);
+	adf4159Spi1Tx(PLL_REG_R5_DEVIATION, 1L << R5_SHIFT_DEV_SEL);							// second deviation register is not used
+	adf4159Spi1Tx(PLL_REG_R4_CLOCK, 0L << R4_SHIFT_CLK_DIV_SEL);
+	adf4159Spi1Tx(PLL_REG_R4_CLOCK, 1L << R4_SHIFT_CLK_DIV_SEL);
+	adf4159Spi1Tx(PLL_REG_R3_FUNCTION, (6L << R3_SHIFT_NEG_BLEED_CURRENT)				// negative bleed current 454.7 uA; but not actually enabled
+									| (1L << R3_SHIFT_RESERVED_1)						// reserved; must be 1
+									| (1L << R3_SHIFT_LOL)								// disable "loss of lock" feature; not using it makes things supposedly more robust
+									| (1L << R3_SHIFT_N_SEL)							// 1 = delay N word load by 4 cycles; makes transition between sweeps smoother
+									| (1L << R3_SHIFT_LDP)								// "lock detect precision"; writing 1makes things supposedly more robust
+									| (1L << R3_SHIFT_SIGMA_DELTA_RESET)				// 1 = disable sigma-delta reset on every write
+									| (0L << R3_SHIFT_PD_POL));							// phase detector polarity is negativ
+	adf4159Spi1Tx(PLL_REG_R2_R_DIVIDER, (7L << R2_SHIFT_CP_CURRENT_SETTING)				// CP current 2.5 mA
+									| (R2_REFERENCE_DIV2_VALUE << R2_SHIFT_RDIV2)
+									| (R2_REF_IN_DOUBLER_VALUE << R2_SHIFT_REF_DOUBLER)
+									| (R2_R_COUNTER_VALUE << R2_SHIFT_R_COUNTER)
+									| (0L << R2_SHIFT_CLK1_DIV_VALUE));
 
 	adf4159Spi1Tx(PLL_REG_R1_LSB_FRAC, (0L << R1_SHIFT_LSB_FRACTIONAL_VALUE));			//Fraction LSB is 0 8200
 	adf4159Spi1Tx(PLL_REG_R0_FRAC_INT, (23L << R0_SHIFT_INTEGER_VALUE)				//Int value is 375 18
@@ -341,9 +295,7 @@ static void adf4159Spi1PllConfig(void){
 }
 
 static void adf4159RampSet(void){
-
 	MX_SPI1_ADF4159_Init();
-//	adf4159Spi1Tx(5, 3073829280); //only for test
 	adf4159Spi1Tx(PLL_REG_R7_DELAY, 1L << R7_SHIFT_TX_DATA_TRIGGER); 					//TX_DATA_TRIGGER enabled
 	adf4159Spi1Tx(PLL_REG_R6_STEP, 150L << R6_SHIFT_STEP_WORD);//150
 	adf4159Spi1Tx(PLL_REG_R6_STEP, 1L << R6_SHIFT_STEP_SEL); 							// second step register is not used
@@ -353,7 +305,6 @@ static void adf4159RampSet(void){
 	adf4159Spi1Tx(PLL_REG_R4_CLOCK,  (1L << R4_SHIFT_LE_SEL)
 									| (3L << R4_SHIFT_CLK_DIV_MODE)				// Ramp mode is selected
 									| (CLK2_DIV_VALUE << R4_SHIFT_CLK2_DIV_VALUE));
-//	adf4159Spi1Tx(PLL_REG_R4_CLOCK, 1L << R4_SHIFT_CLK_DIV_SEL);
 	adf4159Spi1Tx(PLL_REG_R4_CLOCK, (1L << R4_SHIFT_LE_SEL)					// enable synchronization of LE pin; avoids timing problems in chip
 									| (3L << R4_SHIFT_CLK_DIV_MODE)				// Clock is used for ramps
 									| (CLK2_DIV_VALUE << R4_SHIFT_CLK2_DIV_VALUE)
@@ -377,20 +328,13 @@ static void adf4159RampSet(void){
 										| (1792L << R0_SHIFT_MSB_FRACTIONAL_VALUE));		//Fraction MSB is 1792 3072 1788
 
 
-//		MX_SPI1_BGT24_Init();
-//		Spi1Bgt24Tran();
-//		bufferLen = sprintf(uart1BufferTx, "BGT24 was configured\n\r");
-//		HAL_UART_Transmit(&huart1, (uint8_t *)uart1BufferTx, bufferLen, 1000);
-
 	bufferLen = sprintf(uart1BufferTx, "Ramp was setted\n\r");
 	HAL_UART_Transmit(&huart1, (uint8_t *)uart1BufferTx, bufferLen, 1000);
 }
 
-static void bgt24Spi1Config(void){
+static void bgt24Init(void){
 	MX_SPI1_BGT24_Init();
 	Spi1Bgt24Tran();
-	bufferLen = sprintf(uart1BufferTx, "BGT24 was configured\n\r");
-	HAL_UART_Transmit(&huart1, (uint8_t *)uart1BufferTx, bufferLen, 1000);
 }
 
 static void adf4159Spi1Tx(uint32_t adfAddress, uint32_t data){
